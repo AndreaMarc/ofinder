359c085826633ffb92e0c65497878cbcef091242
Andrea Marcelli <a.marcelli.ext@teamsystem.com>
2025-12-10 17:40:03 +0100

    Aggiunto channelType

---
+++ BE/Src/MIT.Fwk.Infrastructure/Migrations/20251210162616_AddChannelTypeField.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MIT.Fwk.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChannelTypeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChannelType",
+                table: "Channels",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChannelType",
+                table: "Channels");
+        }
+    }
+}
